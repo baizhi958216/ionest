b1a83a17f0cd2bbc1701f7abed5dbccfb0a4542d
baizhi958216 <1475289190@qq.com>
2023-06-23 12:28:58 +0800

    feat: 获取番剧列表
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getList() {
+    const { data } = await fetch('https://api.okmiku.com/bangumi_list', {
+      method: 'POST',
+    }).then((res) => res.json());
+    return data;
   }
 }
