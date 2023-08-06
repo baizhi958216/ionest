d40f83177683be6060b4ad1767b444f3120ce811
baizhi958216 <1475289190@qq.com>
2023-08-06 12:02:24 +0800

    feat: Add Authsign
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,10 @@ export class AppService {
   async getList() {
     const { data } = await fetch('https://api.okmiku.com/bangumi_list', {
       method: 'POST',
+      headers: {
+        Authsign:
+          '5b504b3148da97cb77974af645bf1a592732fe2b1be11356b48c0122f9b37a9c.U2FsdGVkX1+acQkwKsWWUbuk/hYCSkBxcp6mthUpN+4=.MzU4NDkyNjI2OQ==',
+      },
     }).then((res) => res.json());
     return data;
   }
